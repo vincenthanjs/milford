5dcd12e7bc6d64b90c7025d7ac1e82016a7a555b
root <root@localhost.localdomain>
2021-11-06 13:09:50 +1100

    update

---
+++ api/testing/get_cloud.sh
@@ -0,0 +1,3 @@
+curl -k --location --request GET 'https://10.115.1.52/api/cloud' \
+--header 'Authorization: Basic YWRtaW46Vk13YXJlMSE=' \
+--header 'Cookie: avi-sessionid=None; sessionid=None' | jq .
